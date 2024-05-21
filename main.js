50dd77fe9780126179f3baf5be7e48f3c5baefbd
SaidamirRahimtorayev <saidamirxonrahimtorayev@gmail.com>
2024-05-21 18:17:48 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let a = prompt("Ismingizni kiriting...")
+let b = prompt("Space Id kiriting...")
+let c = prompt("Space password kiriting...")
+
+let Space = {
+    name: a,
+    id: b,
+    password: c,
+}
+console.log(Space);
